4a57df6d7125bedf224de18a76c046cc8458cd3c
dh1ee <dhorselee@naver.com>
2018-06-14 10:00:24 +0900

    헬프미

--- Final_Convenience/src/main/java/com/proj/rup/HomeController.java
+++ Final_Convenience/src/main/java/com/proj/rup/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * 
  */
 @Controller
 public class HomeController {
@@ -21,6 +21,7 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * 헬프미
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
